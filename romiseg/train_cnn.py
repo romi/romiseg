ee7d24ee622b8b4176fa6e789cdb05610f1a1611
alienor <alienor@avocat.localdomain>
2019-11-21 15:06:51 +0100

    comment

--- romiseg/train_cnn.py
+++ romiseg/train_cnn.py
@@ -61,32 +61,29 @@ learning_rate = param['learning_rate']
 def cnn_train(directory_weights, directory_dataset, label_names, tsboard, batch_size, epochs,
                     model_segmentation_name, Sx, Sy):
     
-
+    #Training board
+    writer = SummaryWriter(tsboard)
+    num_classes = len(label_names)
+    
+    #image transformation for training, can be modified for data augmentation
+    trans = transforms.Compose([
+                                transforms.CenterCrop((Sx, Sy)),
+                                transforms.ToTensor(),
+                                ])
+    
+    #Load images and ground truth
     path_val = directory_dataset + '/val/'
     path_train = directory_dataset + '/train/'
     
-    writer = SummaryWriter(tsboard)
-    
-    
-    num_classes = len(label_names)
-    
-    trans = transforms.Compose([
-    transforms.CenterCrop((Sx, Sy)),
-    transforms.ToTensor(),
-    ])
-    
     image_train, target_train = init_set('', path_train, 'jpg')
     image_val, target_val = init_set('', path_val, 'jpg')
-    
-    print(image_train)
-    
+        
     train_dataset = Dataset_im_label(image_train, target_train, transform = trans)
     val_dataset = Dataset_im_label(image_val, target_val, transform = trans) 
-    
-        
+            
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
     
-
+    #Show input images 
     fig = plot_dataset(train_loader, label_names, batch_size, showit = False) #display training set
     writer.add_figure('Dataset images', fig, 0)
     
@@ -96,24 +93,25 @@ def cnn_train(directory_weights, directory_dataset, label_names, tsboard, batch_
         'val': DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
         }
     
-    
+    #Load model
     model = smp.Unet(model_segmentation_name, classes=num_classes, encoder_weights='imagenet').cuda()
     
-    
+    #Freeze encoder
     a = list(model.children())
     for child in  a[0].children():
         for param in child.parameters():
             param.requires_grad = False
-    
+   
+    #Choice of optimizer, can be changed
     optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate)
-    
+    #make learning rate evolve
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=30, gamma=0.1)
     
-    
+    #Run training
     model = train_model(dataloaders, model, optimizer_ft, exp_lr_scheduler, writer, 
                         num_epochs = epochs, viz = True, label_names = label_names)
+    #save model
     model_name =  model_segmentation_name + os.path.split(directory_dataset)[1] + '_epoch%d.pt'%epochs
-    
     torch.save(model, directory_weights + '/' + model_name)
 
 
