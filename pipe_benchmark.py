b4adda3f3f7b052dee439f6dfe20c748942d5698
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2025-03-25 15:34:13 +0100

    Refactor and cleanup test and benchmark scripts.
    Removed unused imports, redundant comments, and excessive whitespace for better readability and maintainability.
    Updated formatting for consistent style, reorganized imports, and ensured standardized parameter handling across scripts.

--- pipe_benchmark.py
+++ pipe_benchmark.py
@@ -5,39 +5,28 @@ Created on Thu Nov 21 09:18:24 2019
 
 @author: alienor
 """
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 21 09:18:24 2019
 
-@author: alienor
-"""
-import open3d
-
-import os
 import argparse
-import toml
-from PIL import Image
+import os
+
 import numpy as np
-
+import toml
 import torch
-from torch.autograd import Variable
-from torchvision import transforms
-from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DataLoader
-
-from torch.optim import lr_scheduler
-import torch.optim as optim
 import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
 
-#from torchvision import models
-
-from romiseg.utils.train_3D import train_model_voxels, init_set, Dataset_im_label_3D
-from romiseg.utils.train_from_dataset import plot_dataset
 from romiseg.utils import segmentation_model
-
-import romiseg.utils.vox_to_coord as vtc
 from romiseg.utils.generate_volume import generate_volume
+from romiseg.utils.train_3D import Dataset_im_label_3D
+from romiseg.utils.train_3D import init_set
+from romiseg.utils.train_3D import train_model_voxels
+from romiseg.utils.train_from_dataset import plot_dataset
+
+# from torchvision import models
 
 
 default_config_dir = "/home/alienor/Documents/scanner-meta-repository/Scan3D/config/segmentation2d_guitar.toml"
@@ -45,12 +34,9 @@ default_config_dir = "/home/alienor/Documents/scanner-meta-repository/Scan3D/con
 parser = argparse.ArgumentParser(description='Process some integers.')
 
 parser.add_argument('--config', dest='config', default=default_config_dir,
-                    help='config dir, default: %s'%default_config_dir)
-
+                    help='config dir, default: %s' % default_config_dir)
 
 args = parser.parse_args()
-
-
 
 param_pipe = toml.load(args.config)
 
@@ -58,14 +44,12 @@ direc = param_pipe['TrainingDirectory']
 
 path = direc['path']
 directory_weights = path + direc['directory_weights']
-tsboard = path +  direc['tsboard'] + '/full_pipe'
+tsboard = path + direc['tsboard'] + '/full_pipe'
 directory_dataset = path + direc['directory_dataset']
-
 
 param2 = param_pipe['Segmentation2D']
 
 label_names = param2['labels'].split(',')
-
 
 Sx = param2['Sx']
 Sy = param2['Sy']
@@ -76,57 +60,49 @@ batch_size = param2['batch']
 learning_rate = param2['learning_rate']
 model_name = param2['model_name']
 
-
 param3 = param_pipe['Reconstruction3D']
 N_vox = param3['N_vox']
 coord_file_loc = path + param3['coord_file_loc']
 
-
-
 ############################################################################################################################
-
-
 
 
 generate_volume(directory_dataset + '/train/', coord_file_loc, Sx, Sy, N_vox, label_names)
 generate_volume(directory_dataset + '/val/', coord_file_loc, Sx, Sy, N_vox, label_names)
 
-#def cnn_train(directory_weights, directory_dataset, label_names, tsboard, batch_size, epochs,
+# def cnn_train(directory_weights, directory_dataset, label_names, tsboard, batch_size, epochs,
 #                    model_segmentation_name, Sx, Sy):
 
-#Training board
+# Training board
 writer = SummaryWriter(tsboard)
 num_classes = len(label_names)
 
-#image transformation for training, can be modified for data augmentation
+# image transformation for training, can be modified for data augmentation
 trans = transforms.Compose([
-                            transforms.CenterCrop((Sx, Sy)),
-                            transforms.ToTensor(),
-                            ])
+    transforms.CenterCrop((Sx, Sy)),
+    transforms.ToTensor(),
+])
 
-#Load images and ground truth
+# Load images and ground truth
 path_val = directory_dataset + '/val/'
 path_train = directory_dataset + '/train/'
 
 image_train, target_train, voxel_train = init_set('', path_train)
 image_val, target_val, voxel_val = init_set('', path_val)
 
+train_dataset = Dataset_im_label_3D(image_train, target_train, voxel_train, transform=trans)
+val_dataset = Dataset_im_label_3D(image_val, target_val, voxel_val, transform=trans)
 
-    
-train_dataset = Dataset_im_label_3D(image_train, target_train, voxel_train, transform = trans)
-val_dataset = Dataset_im_label_3D(image_val, target_val, voxel_val, transform = trans) 
-        
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
 
-#Show input images 
-fig = plot_dataset(train_loader, label_names, batch_size, showit = False) #display training set
+# Show input images
+fig = plot_dataset(train_loader, label_names, batch_size, showit=False)  # display training set
 writer.add_figure('Dataset images', fig, 0)
 
-   
 dataloaders = {
     'train': DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0),
     'val': DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
-    }
+}
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
@@ -142,41 +118,38 @@ for child in  a[0].children():
     for param in child.parameters():
         param.requires_grad = False
 '''
-   
-voxels = torch.load(coord_file_loc + '/voxels.pt').to(device)
-      
-model = segmentation_model.ResNetUNet_3D(num_classes, coord_file_loc).to(device)
 
+voxels = torch.load(coord_file_loc + '/voxels.pt').to(device)
+
+model = segmentation_model.ResNetUNet_3D(num_classes, coord_file_loc).to(device)
 
 # freeze backbone layers
 for l in model.base_layers:
     for param in l.parameters():
         param.requires_grad = False
-   
-#Choice of optimizer, can be changed
+
+# Choice of optimizer, can be changed
 optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate)
-#make learning rate evolve
+# make learning rate evolve
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=30, gamma=0.1)
 
-#Run training
+# Run training
 w_back = 1
 w_class = 30
-weights = [w_back] + [w_class]*(num_classes-1) #[ 1 / number of instances for each class]
+weights = [w_back] + [w_class] * (num_classes - 1)  # [ 1 / number of instances for each class]
 class_weights = torch.FloatTensor(weights).cuda()
-
-
-
 
 voxel_loss = nn.CrossEntropyLoss(weight=class_weights)
 
-ext_name = '_segmentation_' + str(Sx) + '_' + str(Sy) + '_epoch%d.pt'%epochs
+ext_name = '_segmentation_' + str(Sx) + '_' + str(Sy) + '_epoch%d.pt' % epochs
 new_model_name = model_name + ext_name
 
 if True:
-    model = train_model_voxels('Segmentation', dataloaders, model, optimizer_ft, exp_lr_scheduler, writer, voxel_loss, voxels,
-                        num_epochs = epochs, viz = True, label_names = label_names)
-        
-    #model[0].save_state_dict(directory_weights + '/' + new_model_name)
+    model = train_model_voxels('Segmentation', dataloaders, model, optimizer_ft, exp_lr_scheduler, writer, voxel_loss,
+                               voxels,
+                               num_epochs=epochs, viz=True, label_names=label_names)
+
+    # model[0].save_state_dict(directory_weights + '/' + new_model_name)
     torch.save(model, directory_weights + '/' + new_model_name)
     model = model[0]
 else:
@@ -185,11 +158,10 @@ else:
 dataloaders = {
     'train': DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=0),
     'val': DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=0),
-    'test' : DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
-    }
+    'test': DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+}
 
-optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate*0.1)
-
+optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate * 0.1)
 
 model.class_layer[0].weight.data.fill_(0)
 model.class_layer[0].weight.data.fill_diagonal_(1)
@@ -198,10 +170,10 @@ print(model.class_layer[0].weight.data)
 
 model = train_model_voxels('Fullpipe', dataloaders, model, optimizer_ft, exp_lr_scheduler,
                            writer, voxel_loss, voxels,
-                    num_epochs = epochs, viz = True, label_names = label_names)
+                           num_epochs=epochs, viz=True, label_names=label_names)
 
-#save model
-model_segmentation_name =  new_model_name + os.path.split(directory_dataset)[1] + '_epoch%d.pt'%epochs
+# save model
+model_segmentation_name = new_model_name + os.path.split(directory_dataset)[1] + '_epoch%d.pt' % epochs
 torch.save(model, directory_weights + '/' + model_segmentation_name)
 
 '''
@@ -214,15 +186,14 @@ cnn_train(directory_weights, directory_dataset, label_names, tsboard, batch_size
 model = torch.load(directory_weights + '/' + model_segmentation_name)[0].to(device)
 accuracy = []
 
-
 for image, label, voxel in dataloaders['train']:
     image = image.to(device)
     pred_im, pred_vox = model(image)
-    voxel = voxel[0,:,3].unsqueeze(1).long()
-    onehot = torch.zeros((voxel.shape[0],4))
+    voxel = voxel[0, :, 3].unsqueeze(1).long()
+    onehot = torch.zeros((voxel.shape[0], 4))
     onehot = onehot.scatter_(1, voxel, 1)
-    accuracy.append(torch.sum(onehot*pred_vox.cpu())/voxel.shape[0])
+    accuracy.append(torch.sum(onehot * pred_vox.cpu()) / voxel.shape[0])
     del image, label, voxel, onehot, pred_im, pred_vox
     print(accuracy)
-    
+
 print(np.mean(accuracy))
--- test_reconstruction.py
+++ test_reconstruction.py
@@ -3,51 +3,34 @@ Created on Thu Nov 21 09:18:24 2019
 
 @author: alienor
 """
-import open3d
 
-import os
 import argparse
-import toml
-from PIL import Image
+
 import numpy as np
-
 import segmentation_models_pytorch as smp
+import toml
 import torch
-from torchvision import transforms
-from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-from torch.optim import lr_scheduler
-import torch.optim as optim
-#from torchvision import models
-
-from plantdb.commons import io
 from plantdb.commons import fsdb
-
-from romiseg.utils.train_from_dataset import train_model
+from plantdb.commons import io
 from romiseg.utils.dataloader_finetune import plot_dataset
-from romiseg.utils import segmentation_model
 
 import romiseg.utils.vox_to_coord as vtc
 from romiseg.utils.generate_volume import generate_ground_truth
-from romiseg.utils.ply import read_ply, write_ply
+from romiseg.utils.ply import write_ply
 
+# from torchvision import models
 
 
 pcd_loc = '/home/alienor/Documents/blender_virtual_scanner/data/COSEG/guitar/'
 
 default_config_dir = "/home/alienor/Documents/scanner-meta-repository/Segmentation/romiseg/parameters_train.toml"
 
-
 parser = argparse.ArgumentParser(description='Process some integers.')
 
 parser.add_argument('--config', dest='config', default=default_config_dir,
-                    help='config dir, default: %s'%default_config_dir)
-
+                    help='config dir, default: %s' % default_config_dir)
 
 args = parser.parse_args()
-
-
 
 param_pipe = toml.load(args.config)
 
@@ -56,14 +39,12 @@ direc = param_pipe['Directory']
 path = direc['path']
 directory_weights = path + direc['directory_weights']
 model_segmentation_name = direc['model_segmentation_name']
-tsboard = path +  direc['tsboard']
+tsboard = path + direc['tsboard']
 directory_dataset = path + direc['directory_dataset']
-
 
 param2 = param_pipe['Segmentation2D']
 
 label_names = param2['labels'].split(',')
-
 
 Sx = param2['Sx']
 Sy = param2['Sy']
@@ -73,13 +54,12 @@ batch_size = param2['batch']
 
 learning_rate = param2['learning_rate']
 
-
 param3 = param_pipe['Reconstruction3D']
 N_vox = param3['N_vox']
 coord_file_loc = path + param3['coord_file_loc']
 
-generate_ground_truth(directory_dataset + '/val/', pcd_loc, coord_file_loc, 
-                           Sx, Sy, N_vox, label_names)
+generate_ground_truth(directory_dataset + '/val/', pcd_loc, coord_file_loc,
+                      Sx, Sy, N_vox, label_names)
 
 db = fsdb.FSDB(directory_dataset)
 db.connect()
@@ -88,36 +68,35 @@ print('Reference scan used to generate data: ', scan.id)
 
 masks = scan.get_fileset('images')
 
-gt = masks.get_files(query={'channel':'segmentation'})
+gt = masks.get_files(query={'channel': 'segmentation'})
 pred_tot = []
 for i, seg in enumerate(gt):
     seg = io.read_npz(seg)
     class_pred = []
     for j, class_name in enumerate(seg.files):
         class_pred.append(seg[seg.files[j]])
-    class_pred = np.stack(class_pred, axis = 0)
+    class_pred = np.stack(class_pred, axis=0)
     pred_tot.append(class_pred)
 
 pred_tot = torch.Tensor(pred_tot)
 
-pred_tot = pred_tot.permute(0,2,3,1)//255
+pred_tot = pred_tot.permute(0, 2, 3, 1) // 255
 preds_flat = vtc.adjust_predictions(pred_tot)
-
 
 xy_full_flat = torch.load(coord_file_loc + '/coords.pt')
 voxels = torch.load(coord_file_loc + '/voxels.pt')
 
-assign_preds = preds_flat[xy_full_flat].reshape(pred_tot.shape[0], 
-                                        xy_full_flat.shape[0]//pred_tot.shape[0], preds_flat.shape[-1])
-assign_preds = assign_preds[:,:,0:-1]
+assign_preds = preds_flat[xy_full_flat].reshape(pred_tot.shape[0],
+                                                xy_full_flat.shape[0] // pred_tot.shape[0], preds_flat.shape[-1])
+assign_preds = assign_preds[:, :, 0:-1]
 assign_preds = torch.log(assign_preds)
-assign_preds = torch.sum(assign_preds, dim = 0)
+assign_preds = torch.sum(assign_preds, dim=0)
 
-#assign_preds = torch.sum(assign_preds, dim = -1)
-preds_max = torch.max(assign_preds, dim = -1).values
-voxels[:,3] = torch.argmax(assign_preds, dim = -1)
+# assign_preds = torch.sum(assign_preds, dim = -1)
+preds_max = torch.max(assign_preds, dim=-1).values
+voxels[:, 3] = torch.argmax(assign_preds, dim=-1)
 voxels = voxels[preds_max >= 0]
-#voxels = voxels[voxels[:,3] != 0]
+# voxels = voxels[voxels[:,3] != 0]
 
 """
     
@@ -129,10 +108,7 @@ voxels[:,3] = torch.argmax(assign_preds[:,:-1], dim = 1)
 voxels = voxels[voxels[:,3] != 0]
 
 """
-write_ply(coord_file_loc +  '/test_rec.ply', [voxels.numpy()],
-      ['x', 'y', 'z', 'label'])
-    
-    
-
+write_ply(coord_file_loc + '/test_rec.ply', [voxels.numpy()],
+          ['x', 'y', 'z', 'label'])
 
 db.disconnect()
